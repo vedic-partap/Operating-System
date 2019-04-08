f6a25bd80dd05b87c984e9d02579e42e7d974ccd
vernwalrahul <vernwalrahul@gmail.com>
2019-04-08 13:13:09 +0530

    working

--- Assingment_6/myfs_copy.h
+++ Assingment_6/myfs_copy.h
@@ -84,7 +84,7 @@ int create_myfs(int fs_size, int block_size) {
 }
 
 int my_open(string file_name){    // returns fd
-	// cout<<"[MY_OPEN] file_name = "<<file_name<<endl;
+	cout<<"[MY_OPEN] file_name = "<<file_name<<endl;
 	super_block_st *sb = (super_block_st *)myfs_mem;
 
 	my_file *files;
@@ -101,7 +101,7 @@ int my_open(string file_name){    // returns fd
 		}
 	}
 
-	// cout<<"[MY_OPEN] no_files = "<<*no_files<<endl;
+	cout<<"[MY_OPEN] no_files = "<<*no_files<<endl;
 	files[*no_files].name =file_name;
 	files[*no_files].file_size = 0;
 	files[*no_files].start_block = -1;
@@ -141,24 +141,14 @@ int my_read(int fd, int nbytes, char *buf){
 	int curr_block = sb->fd_t[fd]->start_block;
 	int file_size = sb->fd_t[fd]->file_size;
 
-	int seek = sb->fd_t[fd]->seek;
-	while(seek>=sb->block_size) {
-		seek -= sb->block_size;
-		curr_block = block1_ptr->FAT[curr_block].next;
-	}
-	if(curr_block==-1) {
-		sb->fd_t[fd]->seek = -1;
-		return 0;
-	}
-
-	// cout<<"[MY_READ] fd = "<<fd<<", curr_block = "<<curr_block<<endl;
+	cout<<"[MY_READ] fd = "<<fd<<", start_block = "<<curr_block<<endl;
 	char *temp;
-	int bytes_read = 0, to_be_read = min(file_size - sb->fd_t[fd]->seek, nbytes);
-	// cout<<"[MY_READ] to_be_read = "<<to_be_read<<endl;
+	int bytes_read = 0, to_be_read = min(file_size, nbytes);
+	cout<<"[MY_READ] to_be_read = "<<to_be_read<<endl;
 
 	while(bytes_read<to_be_read) {
 		temp = (char *)(myfs_mem+SUPER_BLOCK_MAX_SIZE+sizeof(block_1)+(sb->block_size)*(curr_block-2));
-		int bytes_here = seek;
+		int bytes_here = 0;
 		while(bytes_read<to_be_read) {
 			if(bytes_here>=sb->block_size) {
 				curr_block = block1_ptr->FAT[curr_block].next;
@@ -168,12 +158,6 @@ int my_read(int fd, int nbytes, char *buf){
 			bytes_read++;
 			bytes_here++;
 		}
-	}
-
-	sb->fd_t[fd]->seek += bytes_read;
-	// cout<<"seek = "<<sb->fd_t[fd]->seek<<endl;
-	if(file_size == sb->fd_t[fd]->seek) {
-		sb->fd_t[fd]->seek = -1;
 	}
 	// cout<<"in my_read, buf = "<<buf<<", temp = "<<temp<<"bytes_read = "<<bytes_read<<endl;
 	return bytes_read;
@@ -195,117 +179,6 @@ int get_free_block(int curr_block){
 		block1_ptr->FAT[curr_block].next = alloted_block;
 
 	return alloted_block;
-}
-
-int my_write(int fd, int nbytes, char *buf) {
-	// cout<<"[MY_WRITE] fd = "<<fd<<" \n";
-	// cout<<"[MY_WRITE] nbytes = "<<nbytes<<endl;
-	super_block_st *sb = (super_block_st *)myfs_mem;
-	block_1 * block1_ptr = (block_1 *)(myfs_mem+SUPER_BLOCK_MAX_SIZE);
-
-	if(sb->fd_t[fd]->mode=='n'){
-		cout<<"INVALID FILE DESCRIPTOR! \n";
-		return -1;
-	}
-
-	int curr_block = sb->fd_t[fd]->start_block;
-	int file_size = sb->fd_t[fd]->file_size;
-	int rem_bytes_to_fill = 0;
-
-	// cout<<"[MY_WRITE] got start_block = "<<curr_block<<" file_size = "<<file_size<<endl;
-
-	if(curr_block == -1) {
-		curr_block = get_free_block(-1);
-		file_size = 0;
-		rem_bytes_to_fill = sb->block_size;
-		sb->fd_t[fd]->start_block = curr_block;
-	}
-	else {
-		while(block1_ptr->FAT[curr_block].next!=-1) {
-			curr_block = block1_ptr->FAT[curr_block].next;
-			file_size -= sb->block_size;
-		}
-		rem_bytes_to_fill = sb->block_size - file_size;
-	}
-
-	// cout<<"[MY_WRITE] after check, start_block = "<<sb->fd_t[fd]->start_block<<", curr_block = "<<curr_block<<endl;
-	// cout<<"[MY_WRITE] rem_bytes_to_fill = "<<rem_bytes_to_fill<<endl;
-	int bytes_written = 0;
-	char *temp;
-	while(nbytes>0) {
-		int already_filled = sb->block_size - rem_bytes_to_fill;
-		temp = (char *)(myfs_mem+SUPER_BLOCK_MAX_SIZE+sizeof(block_1)+(sb->block_size)*(curr_block-2));
-		while(rem_bytes_to_fill>0 && nbytes>0) {
-			temp[already_filled] = buf[bytes_written];
-			already_filled++;
-			rem_bytes_to_fill--;
-			nbytes--;
-			bytes_written++;
-		}
-		if(rem_bytes_to_fill>0)
-			break;
-		curr_block = get_free_block(curr_block);
-		if(curr_block==-1) {
-			cout<<"MEMORY FULL! \n";
-			return -1;
-		}
-		rem_bytes_to_fill = sb->block_size;
-		already_filled = 0;
-	}
-
-	sb->fd_t[fd]->file_size += bytes_written;
-	// cout<<"curr_block = "<<curr_block<<", rem_bytes_to_fill = "<<rem_bytes_to_fill<<endl;
-	// cout<<"written data, temp = "<<temp<<endl;
-	return bytes_written;
-}
-
-int my_copy(string file_name) {
-	FILE *fp = fopen(file_name.c_str(), "r");
-	if(fp==NULL) {
-		cout<<"NO SUCH FILE IN DISK (PC)! \n";
-		return -1;
-	}
-	super_block_st *sb = (super_block_st *)myfs_mem;
-	int fd = my_open(file_name);
-	if(fd==-1)
-		return -1;
-	char buf[sb->block_size];
-
-	while(!feof(fp))
-	{
-		bzero(buf,sizeof(buf));
-		int nbytes = fread(buf,1,80,fp);
-		buf[nbytes] = '\0';
-		// cout<<"[MY_COPY] going to write "<<nbytes<<" bytes"<<endl;;
-		int x = my_write(fd, nbytes, buf);
-		if(x==-1)
-			return -1;
-	}
-	fclose(fp);
-	return 1;
-}
-
-int is_eof(int fd){
-	super_block_st *sb = (super_block_st *)myfs_mem;
-	if(sb->fd_t[fd]->seek==-1)
-		return 1;
-	return  0;
-}
-
-int my_cat(string file_name) {
-	char buff[100];
-
-	int fd = my_open(file_name);
-	if(fd==-1) {
-		return -1;
-	}
-
-	while(!is_eof(fd)) {
-		bzero(buff, sizeof(buff));
-		int nbytes = my_read(fd, sizeof(buff), buff);
-		cout<<buff;
-	}
-	return 1;
 }
 
 #endif
